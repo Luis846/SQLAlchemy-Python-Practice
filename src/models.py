270c0c3a47a4e439d969dc04b1e81457d198d871
Luis Rivera <luis.rivera846@gmail.com>
2019-08-08 01:06:56 +0000

    added tables

--- src/models.py
+++ src/models.py
@@ -8,25 +8,52 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Post(Base):
+    __tablename__ = 'posts'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    likes = Column(Integer, primary_key=True)
+    picture = Column(String(250), nullable=False)
+    comments = Column(String(250), nullable=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-    def to_dict(self):
+
+class Edits(Base):
+        __tablename__ = 'edits'
+        id = Column(Integer, primary_key=True)
+        bio = Column(String(250), nullable=True)
+        website = Column(String(250), nullable=True)
+        user_id = Column(Integer, ForeignKey('user.id'))
+        user = relationship(User)
+
+class Private(Base):
+        __tablename__ = 'privateinfo'
+        id = Column(Integer, primary_key=True)
+        email = Column(String(250), nullable=True)
+        phone = Column(Integer, nullable=True)
+        gender = Column(String(250), nullable=True)
+        edits_id = Column(Integer, ForeignKey('edits.id'))
+        edits = relationship(Edits)
+
+        class Followers(Base):
+            __tablename__ = 'followers'
+            id = Column(Integer, primary_key=True)
+            name = Column(String(250), nullable=True)
+            user_id = Column(Integer, ForeignKey('user.id'))
+            user = relationship(User)
+
+
+
+def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
